4d4c44c18988364cc27a5c19cf122d47dfde6360
TaylorColon3 <stuwrkr-tcolon4@mx.uillinois.edu>
2023-04-05 16:56:39 +0000

    commit 1 for 4/5

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,4 +17,20 @@ matching_photos = Photo.where({ :id => url_id })
 
   render({ :template => "photo_templates/show.html.erb" })
 end 
+
+def bali
+  # Parameters: {"toast_id"=>"785"}
+
+  the_id = params.fetch("toast_id")
+
+  matching_photos = Photo.where({ :id => the_id })
+
+  the_photo = matching_photos.at(0)
+
+  #the_photo.destroy
+  
+  the_photo
+
+  render({ :template => "photo_templates/bali.html.erb" })
+end
 end 
